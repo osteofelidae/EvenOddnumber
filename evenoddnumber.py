97e8f19b472d3dc1b19f49e2012ea2f95b64190e
osteofelidae <azrieldelta@gmail.com>
2022-10-16 22:05:02 -0700

    QOL

--- evenoddnumber.py
+++ evenoddnumber.py
@@ -2,14 +2,12 @@ upTo = int(input("Input upper limit... "))
 fileName = input("Input file name... ")
 file = open(fileName + ".py", "w")
 
-file.write("number = int(input('Input number... '))\n")
+file.write("number = int(input('Input number... '))\n\n")
 
 for number in range(int(upTo/2)):
     file.write("if number == " + str(number*2) + ":\n")
-    file.write("\tprint('Number is even.')\n")
-    file.write("\n")
+    file.write("\tprint('Number is even.')\n\n")
     file.write("if number == " + str(number*2+1) + ":\n")
-    file.write("\tprint('Number is odd.')\n")
-    file.write("\n")
+    file.write("\tprint('Number is odd.')\n\n")
     
 file.close()
